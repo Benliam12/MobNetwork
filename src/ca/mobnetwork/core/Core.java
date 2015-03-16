4a2f71aec59b3b1d9bf9286578f70a6f25eca026
Nico344721 <Nico344721@outlook.com>
2015-03-15 21:44:53 -0400

    Core start

---
+++ src/ca/mobnetwork/core/Core.java
@@ -0,0 +1,13 @@
+package ca.mobnetwork.core;
+
+import java.util.logging.Logger;
+
+public class Core {
+	
+	public static Logger logger = Logger.getLogger("minecraft");
+	
+	public void onEnable()
+	{
+		
+	}
+}
